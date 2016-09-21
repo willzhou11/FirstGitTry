97fa8100357429157f94fe731d1748840253e36a
Zhou, Will (CORP) <Will.Zhou@adp.com>
2016-09-20 17:08:49 -0700

    remove the command for TreeNode class

--- LeetCode23MergekSortedLists.java
+++ LeetCode23MergekSortedLists.java
@@ -1,11 +1,12 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
- */
+
+Definition for singly-linked list.
+  
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+ 
 public class Solution {
     public ListNode mergeKLists(ListNode[] lists) {
         if(lists == null || lists.length == 0) return null;
